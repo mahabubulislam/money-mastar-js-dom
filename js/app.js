d61daaba3998bb2d86fb2f8bfaeb53bd09de53b6
Mahabubul Islam <mahabub2k1@gmail.com>
2022-02-17 18:54:14 +0600

    error handled for all function

--- js/app.js
+++ js/app.js
@@ -1,44 +1,57 @@
-                // Function for get input
-function getValue(inputID){
+// Function for get input
+function getValue(inputID) {
     const input = document.getElementById(inputID);
     const inputAmount = Number(input.value);
     return inputAmount;
-  
+
 }
-                    // Expenses Calculate
+// Expenses Calculate
 
 document.getElementById('calculate-btn').addEventListener('click', function () {
     const incomeAmount = getValue('income');
     const foodCost = getValue('food');
     const rentCost = getValue('rent');
-    const clothCost =  getValue('clothes');
-    const expenses = document.getElementById('total-expenses');
-    const totalExpenses=foodCost+rentCost+clothCost;
-    const expensesAmount =expenses.innerText=totalExpenses;
-    const balance = document.getElementById('balance');
-    const balanceAmount = balance.innerText =incomeAmount-expensesAmount;
+    const clothCost = getValue('clothes');
+    if (incomeAmount < 0 || foodCost < 0 || rentCost < 0 || clothCost < 0) {
+        alert('Enter Positive number')
+    }
+    else if (isNaN(incomeAmount) || isNaN(foodCost) || isNaN(rentCost) || isNaN(clothCost)) {
+        alert('Enter number only')
+    }
+    else {
+        const expenses = document.getElementById('total-expenses');
+        const totalExpenses = foodCost + rentCost + clothCost;
+        if (totalExpenses > incomeAmount) {
+            alert("Your expenses can't greater than your income")
+        }
+        else {
+            const expensesAmount = expenses.innerText = totalExpenses;
+            const balance = document.getElementById('balance');
+            const balanceAmount = balance.innerText = incomeAmount - expensesAmount;
+        }
+    }
 })
 
-                 // Saving amount
+// Saving amount
 
-document.getElementById('save-btn').addEventListener('click',function(){
+document.getElementById('save-btn').addEventListener('click', function () {
     const savingValue = getValue('save');
     const saving = document.getElementById('saving-amount');
     const incomeAmount = getValue('income');
-    const savingTotal = incomeAmount*(savingValue/100);
+    const savingTotal = incomeAmount * (savingValue / 100);
     const savingAmount = savingTotal;
     const balance = document.getElementById('balance').innerText;
     const balanceAmount = Number(balance);
     const remainingBalance = document.getElementById('remaining-balance')
-   
-    if (savingAmount>balanceAmount){
+
+    if (savingAmount > balanceAmount) {
         alert("You can't save amount more than your balance")
     }
-    else if(isNaN(savingAmount) || savingValue<0){
+    else if (isNaN(savingAmount) || savingValue < 0) {
         alert('Please Enter only number greater than 0')
     }
-    else{
-        remainingBalance.innerText = balanceAmount-savingTotal;
-        saving.innerText=savingTotal
+    else {
+        remainingBalance.innerText = balanceAmount - savingTotal;
+        saving.innerText = savingTotal
     }
 })
