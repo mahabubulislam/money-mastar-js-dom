62db2a154e092f6d5a8742e53c902340e912b43e
Mahabubul Islam <mahabub2k1@gmail.com>
2022-02-17 17:47:38 +0600

    saving error handled

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
+                // Function for get input
 function getValue(inputID){
     const input = document.getElementById(inputID);
     const inputAmount = Number(input.value);
     return inputAmount;
+  
 }
+                    // Expenses Calculate
 
 document.getElementById('calculate-btn').addEventListener('click', function () {
     const incomeAmount = getValue('income');
@@ -14,31 +17,28 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
     const expensesAmount =expenses.innerText=totalExpenses;
     const balance = document.getElementById('balance');
     const balanceAmount = balance.innerText =incomeAmount-expensesAmount;
- 
-     // const incomeInput = document.getElementById('income');
-    // const foodInput = document.getElementById('food');
-    // const foodCost = Number(foodInput.value);
-    // const rentInput = document.getElementById('rent');
-    // const rentCost = Number(rentInput.value);
-    // const clothesInput = document.getElementById('clothes');
-    // const clothCost = Number(clothesInput.value);
-    // const expenses = document.getElementById('total-expenses');
-    // const totalExpenses=foodCost+rentCost+clothCost;
-    // const expensesAmount =expenses.innerText=totalExpenses;
-    // const balance = document.getElementById('balance');
-    // const balanceAmount = balance.innerText =incomeAmount-expensesAmount; 
 })
 
+                 // Saving amount
 
 document.getElementById('save-btn').addEventListener('click',function(){
     const savingValue = getValue('save');
     const saving = document.getElementById('saving-amount');
     const incomeAmount = getValue('income');
     const savingTotal = incomeAmount*(savingValue/100);
-    const savingAmount = saving.innerText=savingTotal;
+    const savingAmount = savingTotal;
     const balance = document.getElementById('balance').innerText;
-    balanceAmount = Number(balance);
-    remainingBalance = document.getElementById('remaining-balance')
-    remainingBalance.innerText = balanceAmount-savingAmount;
-
+    const balanceAmount = Number(balance);
+    const remainingBalance = document.getElementById('remaining-balance')
+   
+    if (savingAmount>balanceAmount){
+        alert("You can't save amount more than your balance")
+    }
+    else if(isNaN(savingAmount) || savingValue<0){
+        alert('Please Enter only number greater than 0')
+    }
+    else{
+        remainingBalance.innerText = balanceAmount-savingTotal;
+        saving.innerText=savingTotal
+    }
 })
